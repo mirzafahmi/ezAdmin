25ad56be88cea5e96628aae63e71fd46090d9267
mirza <mirzafahmi7@gmail.com>
2023-09-30 23:56:32 +0800

    ver: 0.23.9.30.1 - create edit and delete function for supplier models

--- ezAdmin/purchasing/urls.py
+++ ezAdmin/purchasing/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('purchasing/create_supplier/', SupplierCreateView.as_view(), name='purchasing-supplier-create'),
     path('purchasing/supplier_list/', SupplierListView.as_view(), name='purchasing-supplier-list'),
+    path('purchasing/supplier/<int:pk>/update/', SupplierUpdateView.as_view(), name='purchasing-supplier-update'),
+    path('purchasing/supplier/<int:pk>/delete/', SupplierDeleteView.as_view(), name='purchasing-supplier-delete'),
 ]
--- ezAdmin/purchasing/views.py
+++ ezAdmin/purchasing/views.py
@@ -1,25 +1,54 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import Supplier
 from .forms import SupplierForm
+from django.contrib import messages
 
-class SupplierCreateView(CreateView):
-    model = Supplier
-    form_class = SupplierForm
-    template_name = 'purchasing/supplier_create.html'
-    success_url = reverse_lazy('purchasing-supplier-list')
-
-    def form_valid(self, form):
-        # Customize the form validation or save logic if needed
-        return super().form_valid(form)
-
-class SupplierListView(ListView):
+class SupplierListView(LoginRequiredMixin, ListView):
     model = Supplier
     template_name = 'purchasing/supplier_list.html'
     context_object_name = 'suppliers'  # The variable name in the template
 
     # You can customize the queryset if needed
     def get_queryset(self):
+
         return Supplier.objects.all()
+
+class SupplierCreateView(LoginRequiredMixin,CreateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'purchasing/supplier_create.html'
+    success_url = reverse_lazy('purchasing-supplier-list')
+
+    def form_valid(self, form):
+        supplier_name = form.cleaned_data['company_name']
+        messages.success(self.request, f'{supplier_name} created successfully!')
+
+        return super().form_valid(form)
+
+class SupplierUpdateView(LoginRequiredMixin, UpdateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = 'purchasing/supplier_update.html'
+    success_url = reverse_lazy('purchasing-supplier-list')
+
+    def form_valid(self, form):
+        supplier_name = self.get_object().company_name
+        messages.success(self.request, f'{supplier_name} updated successfully!')
+
+        return super().form_valid(form)
+
+class SupplierDeleteView(LoginRequiredMixin,DeleteView):
+    model = Supplier
+    template_name = 'purchasing/supplier_delete.html'
+    success_url = reverse_lazy('purchasing-supplier-list')
+
+    def delete(self, request, *args, **kwargs):
+        supplier_name = self.get_object().company_name
+        response = super().delete(request, *args, **kwargs)
+        messages.success(self.request, f'{supplier_name} deleted successfully!')
+
+        return response
